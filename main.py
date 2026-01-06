bf3fc9088d157cc67137e032ce40fb4eadd54af0
msumari <suleykhery@gmail.com>
2026-01-06 22:01:12 +0000

    feat: Added more vulnerable python code

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import os
-import subprocess
+import sys
 
 
-def connect_to_database():
-    # 1. TRIGGER FOR GITLEAKS (Hardcoded Secret)
-    # Gitleaks detects patterns like "AKIA..." which are AWS Access Keys.
-    aws_access_key = "XXXX1234567890EXAMPLE"
-    aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+def main():
 
-    print(f"Connecting with {aws_access_key}...")
+    aws_key = "AKIA1234567890EXAMPLE"
 
-
-def ping_website(hostname):
-    # 2. TRIGGER FOR SEMGREP (Command Injection)
-    # Passing user input directly to a shell command is dangerous.
-    # A hacker could send hostname = "google.com; rm -rf /"
-    os.system("ping -c 1 " + hostname)
+    if len(sys.argv) > 1:
+        user_input = sys.argv[1]
+        os.system("ping " + user_input)
 
 
 if __name__ == "__main__":
-    connect_to_database()
-    ping_website("google.com")
+    main()
